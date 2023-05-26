12037e15224c458e47589c5f90eb470393924883
huy09111999 <huytq72@wru.vn>
2023-05-26 11:54:32 +0700

    update README

--- src/addons/mod/mod.module.ts
+++ src/addons/mod/mod.module.ts
@@ -37,6 +37,7 @@ import { AddonModSurveyModule } from './survey/survey.module';
 import { AddonModUrlModule } from './url/url.module';
 import { AddonModWikiModule } from './wiki/wiki.module';
 import { AddonModWorkshopModule } from './workshop/workshop.module';
+import { AddonModThUrlModule } from './th_url/thurl.module';
 
 @NgModule({
     imports: [
@@ -63,6 +64,7 @@ import { AddonModWorkshopModule } from './workshop/workshop.module';
         AddonModUrlModule,
         AddonModWikiModule,
         AddonModWorkshopModule,
+        AddonModThUrlModule
     ],
 })
 export class AddonModModule { }
